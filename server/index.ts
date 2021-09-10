c3aa6bb0e4534c6384cf64f7a90e677f3cd5c949
mmmG <a331998513@gmail.com>
2021-09-11 00:00:36 +0200

    initial setup

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,23 @@
-console.log("hello?");
+import { Request, Response } from "express";
+
+const express = require("express");
+// const cors = require('cors');  //In case cross-origin erro pops up
+
+const authRoutes = require("./routes/auth.ts");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+require("dotenv").config();
+
+// app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("hello I am server!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
