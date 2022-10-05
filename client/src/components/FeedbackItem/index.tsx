7c03870a5aecd6b0957e59ac1a6099ec08badea9
mmmG <a331998513@gmail.com>
2022-10-05 16:49:45 +0200

    Simple debounce added for voting button

--- client/src/components/FeedbackItem/index.tsx
+++ client/src/components/FeedbackItem/index.tsx
@@ -1,21 +1,22 @@
-import { useLocation } from "react-router-dom";
-import { useEffect, useRef } from "react";
-import { useAnimation } from "framer-motion";
-import ArrowUp from "../../assets/shared/icon-arrow-up.svg";
-import CommentIcon from "../../assets/shared/icon-comments.svg";
-import * as F from "./FeedbackItemElements";
-import { useDispatch, useSelector } from "react-redux";
-import { upVote, downVote } from "../../actions";
-import { Comments_type, Item } from "../../Types";
-import { feedbackVariants } from "../../utilities/framerMotion";
-import { motion } from "framer-motion";
+import { useLocation } from 'react-router-dom';
+import { useState, useEffect, useRef } from 'react';
+import { useAnimation } from 'framer-motion';
+import ArrowUp from '../../assets/shared/icon-arrow-up.svg';
+import CommentIcon from '../../assets/shared/icon-comments.svg';
+import * as F from './FeedbackItemElements';
+import { useDispatch, useSelector } from 'react-redux';
+import { upVote, downVote } from '../../actions';
+import { Comments_type, Item } from '../../Types';
+import { feedbackVariants } from '../../utilities/framerMotion';
+import { motion } from 'framer-motion';
 
 const FeedbackItem = (props: Item) => {
+  const [executing, setExecuting] = useState(false);
   const controls = useAnimation();
   const location = useLocation();
   const USER = useSelector((state: any) => state.user);
   const LOGGED = useSelector((state: any) => state.logged);
-  const path = location.pathname.replace("/feedback-detail/", "");
+  const path = location.pathname.replace('/feedback-detail/', '');
   const clickable = props.link.toLowerCase() !== path;
   clickable === true ? controls.start(feedbackVariants.in) : controls.stop();
   const dispatch = useDispatch();
@@ -37,29 +38,40 @@ const FeedbackItem = (props: Item) => {
     }
   }, [clickable]);
 
-  const handleVote = () => {
+  const handleVote = async () => {
     if (!LOGGED) return;
+    setExecuting(true);
+
     if (!isVoted()) {
-      dispatch(upVote(props, props._id, USER));
+      try {
+        dispatch(upVote(props, props._id, USER));
+      } finally {
+        setTimeout(() => setExecuting(false), 1000);
+      }
     } else {
-      dispatch(downVote(props, props._id, USER));
+      try {
+        if (props.vote === 0) return;
+        dispatch(downVote(props, props._id, USER));
+      } finally {
+        setTimeout(() => setExecuting(false), 1000);
+      }
     }
   };
 
   return (
     <motion.li data-clickable={clickable} key={props._id} layout>
-      <F.FeedbackDiv initial={clickable === true ? "initial" : "stop"} animate={controls} variants={feedbackVariants}>
-        <F.Vote data-voted={isVoted()} onClick={handleVote}>
-          <F.VoteIcon width="10" height="7" src={ArrowUp} data-voted={isVoted()} alt="" />
+      <F.FeedbackDiv initial={clickable === true ? 'initial' : 'stop'} animate={controls} variants={feedbackVariants}>
+        <F.Vote data-voted={isVoted()} onClick={handleVote} disabled={executing}>
+          <F.VoteIcon width='10' height='7' src={ArrowUp} data-voted={isVoted()} alt='' />
           {props.vote}
         </F.Vote>
-        <F.SuggestionWrapper to={"/feedback-detail/" + props.link} data-clickable={clickable} ref={skipTab}>
+        <F.SuggestionWrapper to={'/feedback-detail/' + props.link} data-clickable={clickable} ref={skipTab}>
           <F.SuggestionTitle>{props.title}</F.SuggestionTitle>
           <F.SuggestionText>{props.detail}</F.SuggestionText>
           <F.Feature data-no-pointer={true} data-no-space={true} data-text={props.category} />
         </F.SuggestionWrapper>
-        <F.CommentCounter to={"/feedback-detail/" + props.link} data-clickable={clickable} ref={skipTab2}>
-          <F.CommentIcon width="18" height="16" src={CommentIcon} alt="" />
+        <F.CommentCounter to={'/feedback-detail/' + props.link} data-clickable={clickable} ref={skipTab2}>
+          <F.CommentIcon width='18' height='16' src={CommentIcon} alt='' />
           {props.comments.length + countReplies}
         </F.CommentCounter>
       </F.FeedbackDiv>
